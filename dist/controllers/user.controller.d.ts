46d2cc28a4fd718100e56d42846fbe4161efb12f
IsviCastillo <isvijosecastillocruz@outlook.com>
2026-05-08 17:28:34 -0600

    Eliminar archivos generados del repositorio

--- dist/controllers/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-export declare class UserController {
-    getUsers(req: Request, res: Response): Promise<void>;
-    getProfile(req: Request, res: Response): Promise<void>;
-    createUser(req: Request, res: Response): Promise<void>;
-    updateUser(req: Request, res: Response): Promise<void>;
-    deleteUser(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=user.controller.d.ts.map
--- dist/entities/user.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class User {
-    id: string;
-    nombre: string;
-    email: string;
-    password: string;
-}
-//# sourceMappingURL=user.entity.d.ts.map
